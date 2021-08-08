dc04a5cfc96e4b5611acadfea985bfb1a9dab01c
Dmitriy <veddmitriy9241@gmail.com>
2021-08-08 20:43:44 +0300

    uslovia

--- javascript.js
+++ javascript.js
@@ -1,21 +1,36 @@
 'use strict';
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?');
+// const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?');
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-     b = prompt('На сколько оцените его?', ''),
-    c = prompt('Один из последних просмотренных фильмов?', ''),
-    d = prompt('На сколько оцените его?', '');
+// const a = prompt('Один из последних просмотренных фильмов?', ''),
+//      b = prompt('На сколько оцените его?', ''),
+//     c = prompt('Один из последних просмотренных фильмов?', ''),
+//     d = prompt('На сколько оцените его?', '');
 
-    personalMovieDB.movies[a] = b;
-    personalMovieDB.movies[c] = d;
+//     personalMovieDB.movies[a] = b;
+//     personalMovieDB.movies[c] = d;
 
+// if (4 == 9) {
+//     console.log('ok');
+// } else {
+//     console.log('error');
+// }
 
+const num = 40;
 
+switch (num) {
+    case 49: console.log('Wrong');
+    break;
+    case 100: console.log('Wrong');
+    break;
+    case 50: console.log('Good');
+    break;
+    default: console.log('Not this time');
+}
