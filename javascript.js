e810f26965a1f7e709336e65b9250fd630b70466
Dmitriy <veddmitriy9241@gmail.com>
2021-08-23 12:04:36 +0300

    objectDestruct

--- javascript.js
+++ javascript.js
@@ -1,24 +1,33 @@
-'use strict';
+"use strict";
 
-function first() {
-    // Do something
-    setTimeout(function() {
-        console.log(1);
-    },500);
-}
-function second() {
-    console.log(2);
-}
-first();
-second();
+const options = {
+    name: "test",
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    },
+    makeTest: function(){
+        console.log('test');
+    }
+};
+// options.makeTest();
 
-function learnJavaScript(lang, callBack) {
-    console.log(`Я учу: ${lang}`);
-    callBack();
-}
+const {border,bg} = options.colors;
+console.log(border);
 
-function done(){
-    console.log("Я прошел этот урок");
-}
 
-learnJavaScript('JS', done);
+// let counter = 0;
+// for (let key in options) {
+//     if (typeof(options[key]) === 'object'){
+//         for(let i in options[key]){
+//             console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+//             counter++;
+//         }
+//     } else {
+//         console.log(`Свойство ${key} имеет значение ${options[key]}`);
+//         counter++;
+//     }
+// }
+// console.log(counter);
