a2a8d9effbb7b93866bdc2ea2e11e552486341bf
Dmitriy <veddmitriy9241@gmail.com>
2021-08-24 09:50:59 +0300

    linkOrValueAndSpread

--- javascript.js
+++ javascript.js
@@ -1,34 +1,86 @@
 "use strict";
 
-const arr = [60,200,34,6,8];
+let a = 5,
+    b = a;
 
-arr.sort(compareNum);
-console.log(arr);
+    b = b +5;
+    console.log(b);
+    console.log(a);
 
-function compareNum(a, b) {
-    return a-b;
-}
+    const obj = {
+        a: 5,
+        b: 1
+    };
 
-// arr.forEach(function(item, i, arr){
-//    console.log(`${i}: ${item} внутри массива ${arr}`);
-// });
+    // const copy = obj;//Ссылкана ужесуществующий объект
+    // copy.a = 10;
+    // console.log(copy);
+    // console.log(obj);
 
+    function copy(mainObj){
+        let objCopy = {};
+        let key;
+        for (key in mainObj){
+            objCopy[key] = mainObj[key];
+        }
+        return objCopy;
+    }
+    
+    const numbers = {
+        a: 2,
+        b: 5,
+        c: {
+            x: 7,
+            y: 4
+        }
+    };
 
-// arr[99] = 0;
-// console.log(arr);
+    // const newNumbers = copy(numbers);
+    // newNumbers.c.x = 10;
+    // console.log(newNumbers);
+    // console.log(numbers);
 
+    const add = {
+        d: 17,
+        e: 20
+    };
+    
+    const clone = Object.assign({}, add);
+    clone.d = 26;
+    // console.log(add);
+    // console.log(clone);
 
+    const oldArray = ['a','b','c'];
+    const newArray = oldArray.slice();
 
-// for (let i = 0; i < arr.length; i++) {
-//     console.log(arr[i]);
-// }
+    newArray[0] = 'gh';
 
+    console.log(newArray);
+    console.log(oldArray);
 
-// for (let value of arr) {
-//     console.log(value);
-// }
+    const video = ['youtube', 'vimeo', 'rutube'],
+        blogs = ['wordpress', 'livejournal', 'blogger'],
+        internet = [...video, ...blogs, 'vk', 'facebook'];
 
-const str = prompt("","");
-const products = str.split(", ");
-products.sort();
-console.log(products.join('; '));
+    console.log(internet);
+
+    function log(a, b, c){
+        console.log(a);
+        console.log(b);
+        console.log(c);
+    }
+    const num = [2, 5, 7];
+    log(...num);
+
+    const array = ['a', 'b'];
+    const newArray = [...array];
+
+    const q = {
+        one: 1,
+        two: 2
+    };
+
+    const newObj = {...q};
+    newObj.one = 23;
+    console.log(newObj);
+    console.log(q);
